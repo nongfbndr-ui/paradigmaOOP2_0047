30a011a232f511965e534b912fc3a353a315a366
Moreno <nongfbndr@gmail.com>
2026-05-18 12:44:19 +0700

    menambahkan class budi

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -28,3 +28,15 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur), 
+        pelajar(pUmur), 
+        orang(pUmur) // ha ini dapat dilakukan jika menggunakan virtual
+        {
+            cout << "Budi dibuat\n" << endl;
+        }
+};
+
